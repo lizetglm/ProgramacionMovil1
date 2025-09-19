abe08bd85852d6ea4d06b3fadb95df9bfc527d38
lizetglm <lizetglm@gmail.com>
2025-09-18 21:09:59 -0600

    IU basada en el estado

--- U4/BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
+++ U4/BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
@@ -17,8 +17,14 @@ fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
         //val count: MutableState<Int> = remember { mutableStateOf(0) }
         var count by remember { mutableStateOf(0) }
-        Text("You've had $count glasses.")
-        Button(onClick = { count++ }, Modifier.padding(top = 8.dp)) {
+        if (count > 0) {
+            Text("You've had $count glasses.")
+        }
+        Button(
+            onClick = { count++ },
+            Modifier.padding(top = 8.dp),
+            enabled = count<10)
+        {
             Text("Add one")
         }
     }
