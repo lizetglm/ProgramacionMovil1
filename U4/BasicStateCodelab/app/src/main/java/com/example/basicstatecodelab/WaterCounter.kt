a8c98014e1496d67c3c737c2c6eb16e4a837ecdb
lizetglm <lizetglm@gmail.com>
2025-09-18 22:07:17 -0600

    Cómo recordar en la composición

--- U4/BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
+++ U4/BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
@@ -1,4 +1,5 @@
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -15,17 +16,40 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        //val count: MutableState<Int> = remember { mutableStateOf(0) }
         var count by remember { mutableStateOf(0) }
         if (count > 0) {
             Text("You've had $count glasses.")
         }
-        Button(
-            onClick = { count++ },
-            Modifier.padding(top = 8.dp),
-            enabled = count<10)
-        {
+        Button(onClick = { count++ }, Modifier.padding(top = 8.dp), enabled = count < 10) {
             Text("Add one")
         }
     }
 }
+/*fun WaterCounter(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(16.dp)) {
+        //val count: MutableState<Int> = remember { mutableStateOf(0) }
+        var count by remember { mutableStateOf(0) }
+        if (count > 0) {
+            var showTask by remember { mutableStateOf(true) }
+            if(showTask){
+                WellnessTaskItem(
+                    onClose = {showTask=false},
+                    taskName = "Have you taken your 15 minute walk today?"
+                )
+            }
+            Text("You've had $count glasses.")
+        }
+        Row {
+            Button(onClick = { count++ },Modifier.padding(top = 8.dp),enabled = count<10
+            ){
+                Text("Add one")
+            }
+            Button(onClick = { count= 0},Modifier.padding(top = 8.dp)
+            ){
+                Text("Clear water count")
+            }
+        }
+
+    }
+}
+ */
